eb6d14c6b2d83462c0f8d43d85ec8418414843e0
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-09-23 22:05:50 +0300

    Update cp.c

--- seminar_3/cp.c
+++ seminar_3/cp.c
@@ -1,10 +1,10 @@
 #define _GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
-#include <libgen.h>
+#include <getopt.h>
 #include <limits.h>
-#include <stdbool.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,7 +30,6 @@ static void eprintf(const char *fmt, ...) {
     va_end(ap);
     fputc('\n', stderr);
 }
-
 static void eperror(const char *fmt, ...) {
     va_list ap;
     fputs("cp: ", stderr);
@@ -40,183 +39,117 @@ static void eperror(const char *fmt, ...) {
     fprintf(stderr, ": %s\n", strerror(errno));
 }
 
-static const char* basename_const(const char* p) {
-    const char* slash = strrchr(p, '/');
-    return slash ? slash + 1 : p;
+static inline const char* base(const char *p) {
+    const char *s = strrchr(p, '/');
+    return s ? s + 1 : p;
 }
-
-static bool is_directory(const char *path) {
+static inline bool is_dir(const char *p) {
     struct stat st;
-    return stat(path, &st) == 0 && S_ISDIR(st.st_mode);
+    return stat(p, &st) == 0 && S_ISDIR(st.st_mode);
 }
-
-static bool same_inode(const struct stat* a, const struct stat* b) {
+static inline bool same_file(const struct stat *a, const struct stat *b) {
     return a->st_ino == b->st_ino && a->st_dev == b->st_dev;
 }
 
-static void usage(const char* prog) {
-    fprintf(stderr,
-            "cp: invalid option %s\n", prog);
-}
-
-static bool confirm_overwrite(const char *dst) {
+static bool ask_overwrite(const char *dst){
     fprintf(stderr, "cp: overwrite '%s'? ", dst);
     fflush(stderr);
-
-    int c = EOF;
-    c = getchar();
-    while (c != '\n' && c != EOF) {
-        int last = c;
-        c = getchar();
-        if (c == '\n' || c == EOF) { c = last; break; }
-    }
-    return c == 'y' || c == 'Y';
+    int c = getchar();
+    int d; while ((d = getchar()) != '\n' && d != EOF) {}
+    return c=='y' || c=='Y';
 }
 
-static int copy_regular(const char *src, const char *dst, const Options *opt) {
-    struct stat sst;
-    if (stat(src, &sst) < 0) {
-        eperror("cannot stat '%s'", src);
-        return -1;
-    }
-    if (!S_ISREG(sst.st_mode)) {
-        eprintf("omitting non-regular file '%s'", src);
-        return -1;
-    }
+static int copy1(const char *src, const char *dst, const Options *opt) {
+    struct stat ss, ds;
+    if (stat(src, &ss) < 0) { eperror("cannot stat '%s'", src); return -1; }
+    if (!S_ISREG(ss.st_mode)) { eprintf("omitting non-regular file '%s'", src); return -1; }
 
-    struct stat dstst;
-    bool dst_exists = (stat(dst, &dstst) == 0);
-
-    if (dst_exists && S_ISDIR(dstst.st_mode)) {
-        eprintf("cannot overwrite directory '%s' with non-directory", dst);
-        return -1;
-    }
-
-    if (dst_exists && same_inode(&sst, &dstst)) {
-        eprintf("'%s' and '%s' are the same file", src, dst);
-        return -1;
-    }
+    bool dst_exists = (stat(dst, &ds) == 0);
+    if (dst_exists && S_ISDIR(ds.st_mode)) { eprintf("cannot overwrite directory '%s' with non-directory", dst); return -1; }
+    if (dst_exists && same_file(&ss, &ds)) { eprintf("'%s' and '%s' are the same file", src, dst); return -1; }
 
     if (dst_exists) {
-        if (opt->interactive && !confirm_overwrite(dst)) {
-            return 0;
-        }
-        if (opt->force) {
-            (void)unlink(dst);
-        }
+        if (opt->interactive && !ask_overwrite(dst)) return 0;  
+        if (opt->force) (void)unlink(dst);                      
     }
 
-    int sfd = open(src, O_RDONLY);
-    if (sfd < 0) {
-        eperror("cannot open '%s' for reading", src);
-        return -1;
-    }
+    int in  = open(src, O_RDONLY);
+    if (in  < 0) { eperror("cannot open '%s' for reading", src); return -1; }
+    int out = open(dst, O_WRONLY|O_CREAT|O_TRUNC, ss.st_mode & 0777);
+    if (out < 0) { int e=errno; close(in); errno=e; eperror("cannot create regular file '%s'", dst); return -1; }
 
-    mode_t mode = sst.st_mode & 0777;
-    int dfd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, mode);
-    if (dfd < 0) {
-        int saved = errno;
-        close(sfd);
-        errno = saved;
-        eperror("cannot create regular file '%s'", dst);
-        return -1;
-    }
-
-    char buf[128 * 1024];
+    char buf[128*1024];
     int rc = 0;
-
     while (1) {
-        ssize_t n = read(sfd, buf, sizeof buf);
+        ssize_t n = read(in, buf, sizeof buf);
         if (n == 0) break;
-        if (n < 0) { eperror("error reading '%s'", src); rc = -1; break; }
-
-        ssize_t off = 0;
-        while (off < n) {
-            ssize_t m = write(dfd, buf + off, (size_t)(n - off));
-            if (m <= 0) { eperror("error writing '%s'", dst); rc = -1; off = n; break; }
+        if (n < 0) { eperror("error reading '%s'", src); rc=-1; break; }
+        for (ssize_t off=0; off<n; ) {
+            ssize_t m = write(out, buf+off, (size_t)(n-off));
+            if (m <= 0) { eperror("error writing '%s'", dst); rc=-1; break; }
             off += m;
         }
-        if (rc != 0) break;
+        if (rc) break;
     }
 
-    int err_close_src = close(sfd);
-    int err_close_dst = close(dfd);
-    if (rc == 0 && err_close_dst != 0) {
-        eperror("failed to close '%s'", dst);
-        rc = -1;
-    }
-    (void)err_close_src;
+    if (close(in)  != 0) rc = -1;
+    if (close(out) != 0) rc = -1;
 
-    if (rc == 0 && opt->verbose) {
-        printf("'%s' -> '%s'\n", src, dst);
-        fflush(stdout);
-    }
+    if (!rc && opt->verbose) printf("'%s' -> '%s'\n", src, dst);
     return rc;
 }
 
-static void build_target_in_dir(char out[PATH_MAX], const char* dir, const char* src) {
-    const char* name = basename_const(src);
-    size_t len = snprintf(out, PATH_MAX, "%s/%s", dir, name);
-    if (len >= PATH_MAX) {
-        out[PATH_MAX - 1] = '\0';
-    }
+static void usage(const char *prog){
+    fprintf(stderr,
+        "Usage: %s [OPTION]... SOURCE DEST\n"
+        "  or:  %s [OPTION]... SOURCE... DIRECTORY\n"
+        "Options:\n"
+        "  -v, --verbose       explain what is being done\n"
+        "  -i, --interactive   prompt before overwrite\n"
+        "  -f, --force         remove destination files before copying\n",
+        prog, prog);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv) {
     Options opt = {0};
 
-    int idx = 1;
-    for (; idx < argc; ++idx) {
-        const char* a = argv[idx];
-        if (strcmp(a, "--") == 0) { ++idx; break; }
-        if (a[0] != '-') break;
+    static const struct option longopts[] = {
+        {"verbose",     no_argument, 0, 'v'},
+        {"interactive", no_argument, 0, 'i'},
+        {"force",       no_argument, 0, 'f'},
+        {0,0,0,0}
+    };
 
-        if (strcmp(a, "-v") == 0 || strcmp(a, "--verbose") == 0) {
-            opt.verbose = true;
-        } else if (strcmp(a, "-i") == 0 || strcmp(a, "--interactive") == 0) {
-            opt.interactive = true;  
-            opt.force = false;
-        } else if (strcmp(a, "-f") == 0 || strcmp(a, "--force") == 0) {
-            opt.force = true;
-            opt.interactive = false;
-        } else {
-            usage(a);
-            return 1;
+    int c;
+    while ((c = getopt_long(argc, argv, "vif", longopts, NULL)) != -1) {
+        switch (c) {
+            case 'v': opt.verbose = true; break;
+            case 'i': opt.interactive = true; opt.force = false; break;
+            case 'f': opt.force = true;       opt.interactive = false; break;
+            case '?': usage(argv[0]); return 1;
         }
     }
 
-    int n_paths = argc - idx;
-    if (n_paths < 2) {
-        usage(argv[0]);
-        return 1;
-    }
+    int n_paths = argc - optind;
+    if (n_paths < 2) { usage(argv[0]); return 1; }
 
-    int exit_code = 0;
-
+    int rc = 0;
     if (n_paths == 2) {
-        const char *src = argv[idx];
-        const char *dst = argv[idx + 1];
-
-        if (is_directory(dst)) {
-            char full[PATH_MAX];
-            build_target_in_dir(full, dst, src);
-            if (copy_regular(src, full, &opt) != 0) exit_code = 1;
+        const char *src = argv[optind];
+        const char *dst = argv[optind + 1];
+        if (is_dir(dst)) {
+            char to[PATH_MAX]; snprintf(to, sizeof to, "%s/%s", dst, base(src));
+            if (copy1(src, to, &opt) != 0) rc = 1;
         } else {
-            if (copy_regular(src, dst, &opt) != 0) exit_code = 1;
+            if (copy1(src, dst, &opt) != 0) rc = 1;
         }
     } else {
         const char *dir = argv[argc - 1];
-        if (!is_directory(dir)) {
-            eprintf("target '%s' is not a directory", dir);
-            return 1;
-        }
-
-        for (int i = idx; i < argc - 1; ++i) {
-            char full[PATH_MAX];
-            build_target_in_dir(full, dir, argv[i]);
-            if (copy_regular(argv[i], full, &opt) != 0) exit_code = 1;
+        if (!is_dir(dir)) { eprintf("target '%s' is not a directory", dir); return 1; }
+        for (int i = optind; i < argc - 1; ++i) {
+            char to[PATH_MAX]; snprintf(to, sizeof to, "%s/%s", dir, base(argv[i]));
+            if (copy1(argv[i], to, &opt) != 0) rc = 1;
         }
     }
-
-    return exit_code;
+    return rc;
 }
